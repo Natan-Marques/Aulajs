dc7d2644f58031b53ee26d25cba2f7be3913e3fa
Natan Marques <Natamdess@gmail.com>
2019-11-23 18:35:39 -0300

    funcoes

---
+++ Aula.js
@@ -0,0 +1,18 @@
+function multiplicacao (n1, n2) {
+    let num1 = n1
+    const num2 = n2
+
+    console.log(num1 * num2)
+}
+
+multiplicacao(60, 60)
+
+function divisao (n1, n2) {
+    let num1 = n1
+    const num2 = n2
+
+    console.log(num1 / num2)
+}
+
+divisao(120, 2)
+
