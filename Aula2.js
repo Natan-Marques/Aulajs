f6fb69329e63e21f39b9edee2300f7df059cf0c7
Natan Marques <Natamdess@gmail.com>
2019-11-30 21:14:48 -0300

    Aprendi if e else

---
+++ Aula2.js
@@ -0,0 +1,34 @@
+function imc (peso, altura) {
+let calc = peso/altura*2
+console.log(calc)
+
+}
+//imc(50,1.75)
+
+function recebedorDeEmail (email) {
+let principal = "terezinhadejesus@gmail.com"
+
+if (principal==email) {
+    console.log("Seu endereço foi encontrado em nosso sistema")
+
+}
+else {
+    console.log("Seu e-mail é inválido")
+}
+
+}
+//recebedorDeEmail("terexinhadejesus@gmail.com")
+
+function cura (remedio) {
+    let generico = "estomazil"
+    
+    if (generico==remedio) {
+        console.log("Seu fígado foi curado")
+    }
+
+else {console.log("Seu fígado continua doente")
+}
+}
+cura("estomazil")
+
+
